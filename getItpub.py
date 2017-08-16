8628e825fa28f6858fffc22bf7f8770bcdc1d23b
wangy1b <wangy1b@163.com>
2017-08-16 19:10:40 +0800

    对getItpub的一些修改

--- getItpub.py
+++ getItpub.py
@@ -27,7 +27,9 @@ def getAllPage(url):
     # print('*'*50)
     pageDict={}
     i=1
-    while i<=int(lastPageNum):
+    #i=51  #出错，更改带双引号的标题
+    #while i<=int(lastPageNum):
+    while i<=50:
         pageLink='http://www.itpub.net/'+lastPageLink[:-2]+str(i)
         pageNum=i
         # print(pageLink+'  '+str(pageNum))
@@ -49,52 +51,68 @@ def getOnePage(pageUrl):
         onePage.append([textTitle,textLink])
     return onePage
 
+
 def getOneText(titile,link):
     titile=titile.replace('/','')
+    titile = titile.replace('"', '')
     if '：' in titile:
-         newTitle=titile[titile.index('：') + 1:] #获取'PL/SQL Challenge 每日一题：2017-7-26　12c R2: 超长标识符名字'中'：'后的字符串
+         newTitle=titile[titile.index('：201') + 1:] #获取'PL/SQL Challenge 每日一题：2017-7-26　12c R2: 超长标识符名字'中'：'后的字符串
+         newTitle = newTitle.replace('：', '')
+         newTitle = newTitle.replace(':', '')
     else:
         newTitle = titile
-    oneTextPage = requests.get(link, headers=myHeaders)
-    oneTextSoup=BeautifulSoup(oneTextPage.content,'lxml')
-    posts=oneTextSoup.find_all(id=re.compile('^post_[0-9]+')) #只查找了第一条
-    postnum=1
-    for post in posts:
-        # print(post)
-        # print('*'*30)
-        postAuthor=post.table.tr.td.find(class_='authi').text.strip() #作者newkid
-        # print(postAuthor)
-        if postAuthor=='newkid':
-            # print(postnum)
-            if postnum==1:
-                postcontentFirst = post.table.tr.find(class_='t_f')  # 题目
-                #print(postcontentFirst.a)
-                if   postcontentFirst.a is not None:# postcontentFirst.a 为None时，not  None为啥不是True？
-                    beforeLink=postcontentFirst.a.get('href') #历史题目链接
-                    textFirst = postcontentFirst.text
-                    # print(str(postnum) + ' 题目')
-                    # print(postAuthor)
-                    testtextFirst=textFirst.replace('http://www.itpub.net/forum.php?m ... eid&typeid=1808',beforeLink)
-                else:
-                    textFirst = postcontentFirst.text
-                    testtextFirst = textFirst
-                # print(testtextFirst)
-                file=open('PLSQLChallenge\\'+newTitle+'.txt','w',encoding='utf-8')
-                file.write(testtextFirst.lstrip())
-                file.close()
-            else:
-                postcontentMore=post.table.tr.find(class_='t_f') #答案
-                textMore=postcontentMore.text
-                first2text=textMore.strip()[:2]
-                #print(first2text)
-                if first2text=='答案':
+    forPageNum = requests.get(link, headers=myHeaders)
+    forPageNumSoup=BeautifulSoup(forPageNum.content,'lxml')
+    if forPageNumSoup.find(class_='pg') is not None:#判断是否有多页的情况
+        pgnumtext=forPageNumSoup.find(class_='pg').span.text
+        pgnum=int(re.sub('\D', '', pgnumtext))
+    else:
+        pgnum=1
+    #print(pgnum)
+    #print('*'*80)
+    k=1 #控制每题有多页情况
+    postnum = 1 #控制有多少楼
+    while k<=pgnum:
+        newLink=link+'&page='+str(k)
+        oneTextPage = requests.get(newLink, headers=myHeaders)
+        oneTextSoup = BeautifulSoup(oneTextPage.content, 'lxml')
+        posts=oneTextSoup.find_all(id=re.compile('^post_[0-9]+')) #只查找了第一条
+        for post in posts:
+            #print(postnum) #打印当前是多少楼
+            if post.table.tr.td.find(class_='authi') is not  None:#针对被删除用户的过滤
+                postAuthor=post.table.tr.td.find(class_='authi').text.strip() #作者newkid
+            #print(postAuthor)
+            if postAuthor=='newkid': #只取newkid的帖子
+                # print(postnum)
+                if postnum==1:
+                    postcontentFirst = post.table.tr.find(class_='t_f')  # 题目
+                    #print(postcontentFirst.a)
+                    if   postcontentFirst.a is not None:# postcontentFirst.a 为None时，not  None为啥不是True？
+                        beforeLink=postcontentFirst.a.get('href') #历史题目链接
+                        textFirst = postcontentFirst.text
+                        # print(str(postnum) + ' 题目')
+                        # print(postAuthor)
+                        testtextFirst=textFirst.replace('http://www.itpub.net/forum.php?m ... eid&typeid=1808',beforeLink)
+                    else:
+                        textFirst = postcontentFirst.text
+                        testtextFirst = textFirst
+                    #print(testtextFirst)
+                    file=open('PLSQLChallenge\\'+newTitle+'.txt','w',encoding='utf-8')
+                    file.write(testtextFirst.lstrip())
+                    file.close()
+                else: #除了题目之外的回复
+                    postcontentMore=post.table.tr.find(class_='t_f') #答案
+                    textMore=postcontentMore.text
+
                     # print(str(postnum)+' '+first2text)
-                    # print(postAuthor)
-                    # print(textMore)
+                    #print(postAuthor)
+                    #print(textMore)
                     file=open('PLSQLChallenge\\'+newTitle+'.txt','a',encoding='utf-8')
                     file.write('*'*80+textMore)
                     file.close()
-        postnum+=1
+            postnum+=1
+        k+=1
+        time.sleep(2)
 
 
 
